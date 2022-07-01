1b4a410864f0701e9d33acbf5c14bb1fc09d32f4
Francisco Briones <franciscobrioneslavados@gmail.com>
2022-07-01 10:47:29 -0400

    config Unit Test, testing GifItem

--- gif-app/src/components/GiftItem.jsx
+++ gif-app/src/components/GiftItem.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 export const GiftItem = ({ id, title, url }) => {
   return (
     <div className="card">
@@ -5,4 +7,9 @@ export const GiftItem = ({ id, title, url }) => {
       <p>{title}</p>
     </div>
   )
+}
+
+GiftItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
---
+++ gif-app/tests/components/GifItem.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import { GiftItem } from '../../src/components'
+
+describe('Pruebas en <GifItem />', () => {
+  const title = 'Saitama'
+  const url = 'https://saitama.com'
+
+  test('debe de jacer match con el snapshot', () => {
+    const { container } = render(<GiftItem title={title} url={url} />)
+    expect(container).toMatchSnapshot()
+  })
+
+  test('debe de mostrar la imagen con el url y el alt indicado', () => {
+    render(<GiftItem title={title} url={url} />)
+    // screen.debug();
+    // expect(screen.getByRole('img').src).toBe(url + "/")
+    const { src, alt } = screen.getByRole('img')
+    expect(src).toBe(url + '/')
+    expect(alt).toBe(title)
+  })
+
+  test('debe de mostrar el titulo en el componente', () => {
+    render(<GiftItem title={title} url={url} />)
+    expect(screen.getByText(title)).toBeTruthy();
+  })
+})
